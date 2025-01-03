bbef7341884d8b68d8c7b05049ecd10e8c124ce3
kevinjohannesson <kroon.kevin@gmail.com>
2025-01-03 17:15:29 +0100

    set up basic scene with box

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import { Canvas, MeshProps } from "@react-three/fiber";
+import { Mesh } from "three";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Box(props: MeshProps) {
+  const meshRef = useRef<Mesh>(null!);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <mesh {...props} ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={"orange"} />
+    </mesh>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[0, 0, 0]} />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
